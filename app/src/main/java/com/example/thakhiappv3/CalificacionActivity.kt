7dd3008e644952190786f5f521d3dcfcea43478c
joseftjack18 <54047390+joseftjack18@users.noreply.github.com>
2019-12-02 17:19:43 -0500

    Guardar ubicacion 6

--- app/src/main/java/com/example/thakhiappv3/CalificacionActivity.kt
+++ app/src/main/java/com/example/thakhiappv3/CalificacionActivity.kt
@@ -8,5 +8,8 @@ class CalificacionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calificacion)
+
+
+        //dfgdfgdfgdfgdfg
     }
 }
