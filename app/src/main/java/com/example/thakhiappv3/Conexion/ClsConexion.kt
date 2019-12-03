716bd429add4e576720f6f9bb126320352244869
joseftjack18 <54047390+joseftjack18@users.noreply.github.com>
2019-12-03 18:48:33 -0500

    Guardar ubicacion 7

--- app/src/main/java/com/example/thakhiappv3/Conexion/ClsConexion.kt
+++ app/src/main/java/com/example/thakhiappv3/Conexion/ClsConexion.kt
@@ -2,7 +2,8 @@ package com.example.thakhiappv3.Conexion
 
 class ClsConexion {
     companion object{
-        var url:String="https://thakhi.club/Cliente/"
+        //var url:String="https://thakhi.club/Cliente/"
+        var url:String="http://192.168.43.207:8080/ThakhiWS/Cliente/"
         var dni:String=""
         var nombres:String=""
     }
